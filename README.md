9c7e0ef1cdb7e77c996689a2513a27c62db2527e
Kornel Szurek <konrad.szpyrek@gmail.com>
2018-12-08 14:09:38 +0100

    create project

--- README.md
+++ README.md
@@ -5,7 +5,10 @@
     curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
     sudo apt-get install -y nodejs
     sudo npm install -g @angular/cli
+
+    npm install rxjs@6 rxjs-compat@6 --save
     ```
+
 
 3. Flask
 
---
+++ frontend/src/app/exams/exam.model.ts
@@ -0,0 +1,11 @@
+export class Exam {
+  constructor(
+    public title: string,
+    public description: string,
+    public _id?: number,
+    public updatedAt?: Date,
+    public createdAt?: Date,
+    public lastUpdatedBy?: string,
+  ) {
+  }
+}
