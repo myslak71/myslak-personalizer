13a2764d3cad3714e1b9e9254399344c2499e9be
myslak71 <Zabojca2golebi>
2019-01-08 13:51:27 +0100

    refactored whole backend directory

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from mock import patch
-
-class TestMain(unittest.TestCase):
-    @patch('backend.utils.color.generate_random_color')
-    def test_get_outline_color(self, mocked_generate_random_color):
-        mocked_generate_random_color.return_value = ''#abc123'
-        # self.assertEqual()
