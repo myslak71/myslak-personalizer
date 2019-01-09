e57cc25cfbe6d89d5b6469fde23103d1318d6647
myslak71 <Zabojca2golebi>
2019-01-09 11:18:27 +0100

    Add aptfile

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 
 IMG_PATH = os.path.dirname(os.path.abspath(__file__)) + "/static/img"
 
-DB_URL = 'postgres://gpyygjzbvlaand:afd717b8b69a1f7bc364eee1a018d642c5284c6dc73693fc0253d63609a33e0b@ec2-23-21-86-22.compute-1.amazonaws.com:5432/dlnsrv42ijb8v'
-DB_NAME = 'myslak'
-DB_USER = 'myslak'
-DB_PASSWORD = 'postgres'
+DB_URL = os.environ['DATABASE_URL']
+DB_NAME = os.environ['DATABASE_NAME']
+DB_USER = os.environ['DATABASE_USER']
+DB_PASSWORD = os.environ['DATABASE_PASSWORD']
